f0f54a5944bd125e434036f976a5c0f56866a906
Jun Oh <jun.oh@goodmeasures.com>
2017-12-19 14:31:32 -0500

    YouTube mockup practice using React and Youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import API_KEY from '../config'
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+    constructor(props) {
+        super(props)
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+        this.state = { 
+            videos: [],
+            selectedVideo: null,
+        };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.videoSearch('');
+    } 
+
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });;
+            // this.setSfate({ videos: videos})
+        });
+    }
+
+    
+
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
+        return (
+            <div>
+              <SearchBar onSearchTermChange={videoSearch} /> 
+              <VideoDetail video={this.state.selectedVideo} />
+              <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                videos={this.state.videos} />
+            </div>
+        )
+    };
+}; 
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
